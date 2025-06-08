b0b24c253fc85e5b7a6f52a50d26f9e0b50a07ff
ponderous <100857622+popcorn7822@users.noreply.github.com>
2025-06-08 21:56:45 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => (
   <div className="text-center mt-10 text-xl">
-    <p className="text-blue-700 font-semibold">patient-realtime-system</p>
+    <p className="text-blue-700 font-semibold">Patient Real-Time System</p>
     <p className="mt-2">Go to <a href="/patient" className="text-blue-500 underline">patient</a> or <a href="/staff" className="text-blue-500 underline">staff</a></p>
   </div>
 );
